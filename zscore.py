9bc58cba54503513451abd370b20fc30c8436644
ACER <anilapaloor@gmail.com>
2020-04-17 14:17:54 +0530

    z score

--- zscore.py
+++ zscore.py
@@ -20,6 +20,7 @@ def normalProbabilityDensity(x):
     constant = 1.0 / np.sqrt(2 * np.pi)
     return (constant * np.exp((-x ** 2) / 2.0))
 
+
 repeat = "y"
 while repeat == "y":
     try:
@@ -32,3 +33,25 @@ while repeat == "y":
         print(result)
 
     repeat = input(" press y to continue / any key to exit ")
+
+repeatAlpha = "y"
+while repeatAlpha == "y":
+    try:
+        text = input("enter the value alpha to calculate the  z0 such that P(z<z0)=alpha ")
+        number = float(text)
+    except ValueError:
+        print("Error! This is not a number. Try again.")
+    else:
+        r = 0
+        for z in np.arange(-3.49, 3.5, 0.01):
+            result, _ = quad(normalProbabilityDensity, np.NINF, z)
+            if round(float(result), 4) == round(number, 4):
+                print("the calculated z value is")
+                print(z)
+                r = 1
+                break
+
+        if r == 0:
+            print("Sorry !!!! unable to find , try a different value")
+
+    repeatAlpha = input(" press y to continue / any key to exit ")
