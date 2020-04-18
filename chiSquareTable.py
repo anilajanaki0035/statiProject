feda77f9b7e15f71898531328015d2a462bb8ec4
ACER <anilapaloor@gmail.com>
2020-04-18 22:44:47 +0530

    ChiSquare Table final

--- chiSquareTable.py
+++ chiSquareTable.py
@@ -1,35 +1,10 @@
-import inline as inline
-import matplotlib
+
 from scipy.integrate import quad
 import numpy as np
-import matplotlib.pyplot as plt
-import pandas as pd
 
-repeat = "y"
-while repeat == "y":
-    try:
-        text = input("enter the chi square value  to calculate  P   ")
-        value = float(text)
-    except ValueError:
-        print("Error! This is not a number. Try again.")
-    else:
-        try:
-            text = input("enter the degrees of freedom ")
-            number = int(text) / 2
-        except ValueError:
-            print("Error! This is not a number. Try again.")
-        else:
-            gamma = np.math.gamma(number)
-
-        constant = 1 / (pow(2, number) * gamma)
-
-        ProbabilityDensity = lambda x: constant * np.exp(-x / 2) * pow(x, number - 1)
-        result, _ = quad(ProbabilityDensity, 0, value)
-        print("the P value is ", round(1 - float(result), 2))
-
-    repeat = input(" press y to continue / any key to exit ")
-
-print("******************* calculate the chi square value from given p value and DOF***************")
+print("****************************************************************")
+print("Calculation of chi square value from given p value and DOF")
+print("****************************************************************")
 
 repeat = "y"
 while repeat == "y":
@@ -58,5 +33,32 @@ while repeat == "y":
                     break
             if r == 0:
                 print("unable to find !!!! plz try again !!!!")
+
+    repeat = input(" press y to continue / any key to exit ")
+
+print("****************************************************************")
+print("Calculation of chi square value from given p  and DOF")
+print("****************************************************************")
+repeat = "y"
+while repeat == "y":
+    try:
+        text = input("enter the chi square value   ")
+        value = float(text)
+    except ValueError:
+        print("Error! This is not a number. Try again.")
+    else:
+        try:
+            text = input("enter the degrees of freedom ")
+            number = int(text) / 2
+        except ValueError:
+            print("Error! This is not a number. Try again.")
+        else:
+            gamma = np.math.gamma(number)
+
+        constant = 1 / (pow(2, number) * gamma)
+
+        ProbabilityDensity = lambda x: constant * np.exp(-x / 2) * pow(x, number - 1)
+        result, _ = quad(ProbabilityDensity, 0, value)
+        print("the P value is ", round(1 - float(result), 2))
 
     repeat = input(" press y to continue / any key to exit ")
